f4b96809cd5986c4a2b3cbe0a38e5b4605907856
bradage <bradage.merrill@gmail.com>
2021-01-29 07:48:34 -0700

    chapter 8 started

---
+++ w4/js/main.js
@@ -0,0 +1,14 @@
+//access form
+const form = document.forms[0];
+const forma = document.getElementsByTagname('form')[0];
+const formb = document.forms.search;
+const formc = document.forms['search'];
+
+//accessing elements via the element method
+const [input,button] = form.elements;
+const input1 = form.searchInput;
+const input2 = form['searchInput']
+
+const input = form.elements.searchInput;
+input.addEventListener('focus', () => alert('focused'),false);
+input.addEventListener('blur', () => alert('blurred'),false);
