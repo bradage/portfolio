912fb574bfbb7fa87aa8d5e497b0799104c93fc2
bradage <bradage.merrill@gmail.com>
2021-02-01 07:20:43 -0700

    include chrome debugging

--- w4/js/main.js
+++ w4/js/main.js
@@ -1,14 +1,22 @@
+//@ts-check
 //access form
-const form = document.forms[0];
-const forma = document.getElementsByTagname('form')[0];
-const formb = document.forms.search;
-const formc = document.forms['search'];
+// const form = document.forms[0];
+// const forma = document.getElementsByTagname('form')[0];
+// const formb = document.forms.search;
+// const formc = document.forms['search'];
 
-//accessing elements via the element method
-const [input,button] = form.elements;
-const input1 = form.searchInput;
-const input2 = form['searchInput']
+// //accessing elements via the element method
+// const [input,button] = form.elements;
+// const input1 = form.searchInput;
+// const input2 = form['searchInput']
 
-const input = form.elements.searchInput;
-input.addEventListener('focus', () => alert('focused'),false);
-input.addEventListener('blur', () => alert('blurred'),false);
+// const input = form.elements.searchInput;
+// input.addEventListener('focus', () => alert('focused'),false);
+// input.addEventListener('blur', () => alert('blurred'),false);
+
+//show alert on form submit
+const form = document.forms['search'];
+form.addEventListener ('submit', search, false);
+function search() {
+alert(' Form Submitted');
+}
