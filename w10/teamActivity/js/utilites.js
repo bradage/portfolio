0498adf3de63d1e290c993abafa8aa37b8e184c1
Bradey Merrill <bradage.merrill@gmail.com>
2021-03-10 19:54:26 -0700

    welp we did it.

--- w10/teamActivity/js/utilites.js
+++ w10/teamActivity/js/utilites.js
@@ -1,11 +1,10 @@
-const apiUrl = 'https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2019-01-01&endtime=2019-02-02'
-
-function getJSON(url) {
+export function getJSON(url) {
     return fetch(url)
         .then(function (response) {
             if (!response.ok) {
                 throw Error(response.statusText);
             } else {
+                //console.log(response.json());
                 return response.json();
             }
         })
@@ -13,5 +12,8 @@ function getJSON(url) {
             console.log(error);
         });
 }
-
-console.log(getJSON(apiUrl));
+export const getLocation = function (options) {
+    return new Promise(function (resolve, reject) {
+        navigator.geolocation.getCurrentPosition(resolve, reject, options);
+    });
+};
